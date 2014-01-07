6223ca6dc4b80c7e4d1383b4132b1ed2dcba48d4
Juho Vähä-Herttua <juhovh@iki.fi>
2014-01-07 17:16:47 +0200

    Add an AngularJS test, work in progress

---
+++ public/angular/imakes.js
@@ -0,0 +1,89 @@
+angular.module('imakes', ['ngRoute'])
+
+.factory('messageSearch', function($http) {
+  function getSearch(type) {
+    return function (userid, params) {
+      if (typeof(userid) === 'object') {
+        params = userid;
+        userid = null;
+      }
+      params = params || {};
+      return $http.get('/api/search/'+type+(userid?'/'+userid:''), {params: params});
+    }
+  }
+  return {
+    searchMessages: getSearch('messages'),
+    searchImages: getSearch('images'),
+    searchVideos: getSearch('videos'),
+    searchFavorites: getSearch('favorites')
+  };
+})
+
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/images', {
+      controller: 'ListCtrl',
+      templateUrl: 'messagelist.html',
+    })
+    .when('/videos', {
+      controller: 'ListCtrl',
+      templateUrl: 'messagelist.html',
+    })
+    .when('/mymessages', {
+      controller: 'ListCtrl',
+      templateUrl: 'messagelist.html',
+    })
+    .when('/favorites', {
+      controller: 'ListCtrl',
+      templateUrl: 'messagelist.html',
+    })
+    .when('/popular', {
+      controller: 'ListCtrl',
+      templateUrl: 'messagelist.html',
+    })
+    .otherwise({
+      redirectTo: '/images'
+    });
+})
+
+.controller('ListCtrl', function($scope, $location, $routeParams, messageSearch) {
+  var promise;
+  var params = {limit: 20};
+  if (/^\/images/.test($location.url())) {
+    params.order_by = 'id_desc';
+    promise = messageSearch.searchImages(params);
+  } else if (/^\/videos/.test($location.url())) {
+    params.order_by = 'id_desc';
+    promise = messageSearch.searchVideos(params);
+  } else if (/^\/mymessages/.test($location.url())) {
+    params.order_by = 'favorited_desc,id_desc';
+    promise = messageSearch.searchMessages(8, params);
+  } else if (/^\/favorites/.test($location.url())) {
+    promise = messageSearch.searchFavorites(8);
+  } else if (/^\/popular/.test($location.url())) {
+    params.order_by = 'favorited_desc,id_desc';
+    promise = messageSearch.searchFavorites(params);
+  }
+  if (promise) promise.then(function(result) {
+    angular.forEach(result.data.messages, function(message) {
+      angular.forEach(message.images, function(image) {
+        image.src = '/attachment/'+image.id+'/medium';
+      });
+      angular.forEach(message.videos, function(video) {
+        video.src = '/attachment/'+video.id+'/mp4';
+      });
+    });
+    $scope.result = result.data;
+  });
+})
+.directive('ngPoster', function() {
+  return {
+    priority: 99,
+    link: function(scope, element, attr) {
+      attr.$observe('ngPoster', function(value) {
+        if (!value) return;
+        attr.$set('poster', value);
+      });
+    }
+  };
+})
