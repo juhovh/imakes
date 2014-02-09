2df1b9349d466cdcb038edbcb1b69b24468ab08d
Juho Vähä-Herttua <juhovh@iki.fi>
2014-02-09 02:02:01 +0200

    Update the backup script to not break during transaction

--- tools/backup.sh
+++ tools/backup.sh
@@ -1,6 +1,6 @@
 
 backup_dir="."
-backup_files="config.js imakes.db files"
+backup_files="config.js imakes.db.bak files"
 backup_dest=".."
 
 date=$(date +%Y-%m-%d)
@@ -9,6 +9,8 @@ archive_file="imakes-$date.tgz"
 echo "Backing up $backup_files to $backup_dest/$archive_file"
 date
 echo
+sqlite3 $backup_dir/imakes.db ".backup $backup_dir/imakes.db.bak"
 tar czf $backup_dest/$archive_file -C $backup_dir $backup_files
+rm $backup_dir/imakes.db.bak
 echo
 echo "Backup finished"
